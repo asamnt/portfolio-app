9ddf95a95210c71907b41d2ca888a712121e7dde
asamnt <amitsamant@gmail.com>
2022-09-02 10:06:13 +0530

    some more changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div className='introduction' style={{backgroundImage:`url('./introbg.svg')`}}></div>
+    <div className='introduction flex-with-center'>
+      <div className='centered'>
+        <h1>Amit</h1>
+        <div className='intro-content d-flex justify-content-between'>
+          <p> MERN, Java and Python <br/>Full Stack Developer</p>
+          <button className='primary-button w-50'>Know more</button>
+        </div>
+      </div>
     </div>
   )
 }
